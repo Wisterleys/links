4c6d5bf00f1b1211312a5ef5b70a8742c33d83a8
Wisterleys Sousa <wister.sousa2@gmail.com>
2021-04-01 19:50:13 -0300

    Criado a classe Model e Controller foi refatorado

--- js/Controller/Controller.js
+++ js/Controller/Controller.js
@@ -1,23 +1,12 @@
 class Controller{
     constructor(){
-        this.connect()
+        this._model;
 
     }
-    connect(){
-        // Your web app's Firebase configuration
-        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-        var firebaseConfig = {
-            apiKey: "AIzaSyBO5lyXGawbr2sLZuhWiXnhhztq8fsWwxg",
-            authDomain: "list-of-projects-cbf72.firebaseapp.com",
-            databaseURL: "https://list-of-projects-cbf72-default-rtdb.firebaseio.com",
-            projectId: "list-of-projects-cbf72",
-            storageBucket: "list-of-projects-cbf72.appspot.com",
-            messagingSenderId: "557716302386",
-            appId: "1:557716302386:web:a46f9ab2d5fdafafe62a2b",
-            measurementId: "G-WQPBTF584F"
-        };
-        // Initialize Firebase
-        firebase.initializeApp(firebaseConfig);
-        firebase.analytics();
+    initial(){
+        this.model= new Model()
     }
+    get model(){return this._model}
+    set model(value){this._model=value}
+    
 }

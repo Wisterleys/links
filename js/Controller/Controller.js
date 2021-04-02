6b8da55f8a5c320beda044ea923766a51de230a3
Wisterleys Sousa <wister.sousa2@gmail.com>
2021-04-02 11:45:59 -0300

    Método imp refatorado

--- js/Controller/Controller.js
+++ js/Controller/Controller.js
@@ -20,16 +20,19 @@ class Controller{
         })
     }
     imp(value=false){
-        $('.ulsImp')[0].innerHTML+=`
-        <li class="lisImp">
-            <ul class="menu">
-                <li class="menuEdit"><input type="image" src="img/edit-small.png" alt="edite" class="btnEdit"></li>
-                <li class="menuEdit"><input type="image" src="img/delete.png" alt="edite" class="btnDel"></li>
-            </ul>
-            <p>${value?value:"Digite o nome do projeto..."}</p> 
-        </li>
-    `
-    this.listener()
+        let li = this.createTags({place:$('.ulsImp')[0],tag:"li",class:"lisImp"})
+        let ul = this.createTags({place:li,tag:"ul",class:"menu"})
+        let lii = this.createTags({place:ul,tag:"li",class:"menuEdit"})
+        this.createTags({
+            place:lii,tag:"input",class:"btnEdit",alt:"edite",type:"image",src:"img/edit-small.png"
+        })
+        lii=this.createTags({place:ul,tag:"li",class:"menuEdit"})
+        this.createTags({
+            place:lii,tag:"input",class:"btnDel",alt:"delete",type:"image",src:"img/delete.png"
+        })
+        this.createTags({place:li,tag:"p",insertTag:value?value:"Digite o nome do projeto..."})
+        
+        this.listener()
     }
     blurP(){//Escuta todos os P que tem no DOM e ativado quando perde o foco
         $("p").forEach(el=> {

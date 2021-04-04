5f301764cc511f82fe3d0764ba53ad01a4de7dfc
Wisterleys Sousa <wister.sousa2@gmail.com>
2021-04-04 12:52:38 -0300

    Iniciando teste sem realizar o reload da página

--- js/Controller/Controller.js
+++ js/Controller/Controller.js
@@ -2,6 +2,7 @@ class Controller{
     constructor(){
         this._model;
         this._folder=["folder.png","open-folder.png",false]
+        this._currentFolder;
         this._dataset={key:"",el:""}
         this._height;
         this.initial()
@@ -26,24 +27,30 @@ class Controller{
             t.classList.add("hidden")
         })
         $(tag)[index].classList.remove("hidden")
-        index>-1?setTimeout(()=>$(tag)[index].classList.remove("hidden"),100):0
+        index>-1?$(tag)[index].classList.remove("hidden"):0
     }
     reloadPage(){
         document.location.reload({forcedReload:true});
     }
     listenBtnBack(){
         $("#back").on("click",e=>{
-            this.reloadPage()
+            //this.reloadPage()
+            $('.ulsImp')[0].innerHTML=""
+            $("#back").hidden=true
+            $(".btnAdd")[0].hidden=true
+            this.currentFolder.src="img/"+this.folder[0]
         })
     }
     listenToAllFolders(){
         $(".folders").forEach((folder,i)=>{
             folder.$("input")[0].on("click",f=>{
-                console.log(f.target.src="img/"+this.folder[1])
+                this.currentFolder=f.target
+                f.target.src="img/"+this.folder[1]
                 this.AllHidde(".folders",i)
                 this.folder[2]=true
                 this.realTime()
                 $("#back").hidden=false
+                $(".btnAdd")[0].hidden=false
             })
         })
     }
@@ -183,6 +190,8 @@ class Controller{
         return tag
     }
     //GETs e SETs
+    get currentFolder(){return this._currentFolder}
+    set currentFolder(value){this._currentFolder=value}
     get folder(){return this._folder}
     set folder(value){this._folder=value}
     get height(){return this._height}

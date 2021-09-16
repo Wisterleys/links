5986dd769a96d0b48f59d8400501f58c272cd225
Wisterleys <wister.sousa2@gmail.com>
2021-09-16 20:42:00 -0300

    classe Storage criada

---
+++ js/Model/Storage.js
@@ -0,0 +1,42 @@
+class Storage{
+    constructor(){
+
+    }
+    deleteStorage(json){
+        return new Promise((resolve,reject)=>{
+            firebase.storage().ref("/files").child(json.nameFile).delete()
+            .then(res=>{
+            this.deleteFirebase(json.key)
+                resolve(res)
+            })
+            .catch(erro=>{reject([erro,json])})
+            })
+    }
+    uploadTask(files){
+        let promises=[];
+        [...files].forEach(file => {
+            promises.push(new Promise((resolve,reject)=>{
+                let filename_part = file.name.split(".");
+                let filename = filename_part[0]+"-"+Date.now()+"."+filename_part[1]
+                let fileRef = firebase.storage().ref("/files").child(filename)
+                let task = fileRef.put(file)
+                task.on("state_changed",snapshot=>{
+                    document.querySelector("#progress").hidden=false
+                    document.querySelector("#progress div").style.width=`${this.returnsPercent(snapshot._delegate.bytesTransferred,snapshot._delegate.totalBytes)}%`
+                },erro=>{
+                    reject(erro)
+                },()=>{
+                    task.snapshot.ref.getDownloadURL().then(downloadURL=>{
+                        task.snapshot.ref.updateMetadata({ customMetadata: { downloadURL }}).then(metadata=>{
+                         resolve(metadata)
+                       }).catch( error => {
+                         console.error( 'Error update metadata:', error)
+                         reject( error ) 
+                       })
+                    })
+                })
+            }))
+        });
+        return Promise.all(promises)
+    }
+}

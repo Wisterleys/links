c6f86b11ab202343f36f0f4a7b9809ffcf47a0a6
Wisterleys <wister.sousa2@gmail.com>
2021-10-04 13:04:46 -0300

    Adicionado o background video com sucesso

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 const FIELDS={}
 new Controller()
+let play =document.querySelector("#girar")
+play.addEventListener("click",()=>{
+     document.querySelectorAll(".back-video");
+    document.querySelectorAll(".back-video").forEach(video=>{
+        video.play()
+        .then(e=>play.remove())
+    })
+})
